f0658fe46cea30e480c4709358a403baabb50883
charlie morris <charliemorris.96@gmail.com>
2023-04-29 12:32:21 +0100

    rails new

--- app/helpers/taskmanager_helper.rb
+++
@@ -1,2 +0,0 @@
-module TaskmanagerHelper
-end
--- db/migrate/20230429105615_add_default_value_to_completed.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultValueToCompleted < ActiveRecord::Migration[7.0]
-  def change
-    change_column :completed, :show_attribute, :boolean, default: false
-  end
-end
